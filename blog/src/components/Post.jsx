da9d9a54790d63c732d0332c5cd76b538f714dec
mae-hyeng <topjhoh20@naver.com>
2025-04-26 12:58:15 +0900

    -검색 기능 추가 및 UI 보강 작업 진행

--- blog/src/components/Post.jsx
+++ blog/src/components/Post.jsx
@@ -3,12 +3,27 @@ import "./Post.css";
 import PostPreview from "./PostPreview";
 import { BlogStateContext } from "../App";
 
-const Post = () => {
+const Post = ({ search, searchOption }) => {
   const data = useContext(BlogStateContext);
+
+  const onChangeToSearch = () => {
+    if (search === "") return data;
+    if (searchOption === "contentTitle") {
+      return data.filter((item) =>
+        item.contentTitle.toLowerCase().includes(search.toLowerCase())
+      );
+    } else {
+      return data.filter((item) =>
+        item.content.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+  };
+
+  const filteredData = onChangeToSearch();
 
   return (
     <ul className="Posts">
-      {data.map((item) => (
+      {filteredData.map((item) => (
         <PostPreview key={item.id} item={item} />
       ))}
     </ul>
--- blog/src/components/Posts.jsx
+++ blog/src/components/Posts.jsx
@@ -1,14 +1,32 @@
 import "./Posts.css";
 import About from "./About";
 import Post from "./Post";
+import { useState } from "react";
 
 const Posts = () => {
+  const [search, setSearch] = useState("");
+  const [searchOption, setSearchOption] = useState("contentTitle");
+
+  const onChangeInput = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const onChangeSelect = (e) => {
+    console.log(e.target.value);
+    setSearchOption(e.target.value);
+  };
+
   return (
     <div className="Posts-wrapper">
+      <div className="Posts-search">
+        <select onChange={onChangeSelect}>
+          <option value="contentTitle">제목</option>
+          <option value="content">내용</option>
+        </select>
+        <input onChange={onChangeInput} search={search} />
+      </div>
       <div className="max-width">
-        <h2 className="a11y-hidden">Post</h2>
-        <Post />
-        {/* <About /> */}
+        <Post search={search} searchOption={searchOption} />
       </div>
     </div>
   );
